e4a44f4a6e86acbb0376c51d6270a9e4d69f7026
alstjs37 <minsun987654321@gmail.com>
2024-02-19 16:27:56 +0900

    update hello_mpi.cpp

---
+++ hello_mpi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    // MPI 초기화
+    MPI_Init(&argc, &argv);
+
+    // 현재 프로세스의 랭크와 전체 프로세스 수 얻기
+    int rank, world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // 각 프로세스에서 출력
+    cout << "Hello from process " << rank << " of " << world_size << endl;
+
+    // MPI 종료
+    MPI_Finalize();
+
+    return 0;
+}
+
