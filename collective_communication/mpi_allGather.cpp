aa3456229aa12d348b675c06bbd290b7e2213946
alstjs37 <minsun987654321@gmail.com>
2024-02-20 13:52:01 +0900

    update mpi_allGather.cpp

---
+++ collective_communication/mpi_allGather.cpp
@@ -0,0 +1,45 @@
+// practice mpi_gather
+#include <iostream>
+#include <unistd.h>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    int size, rank;
+    
+    // MPI 초기화
+    MPI_Init(&argc, &argv);
+    
+    // 현재 프로세스의 랭크와 전체 프로세스 수 얻기
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int send_msg, buff[size] = {0, };
+
+    // 각 프로세스에서 allGather할 값: 본인의 rank 값 + 1
+    send_msg = rank + 1;
+    
+    cout << "[BEFORE] RANK: " << rank << " AND I RECV (ALLGATHER): ";
+    for(int i = 0; i < size; i++) cout << buff[i] << " ";
+    cout << endl;
+
+    sleep(1);
+    cout << "[SEND] RANK : " << rank << " AND I WILL SEND ( " << send_msg << " )" << endl;
+    sleep(1);
+
+    // MPI_Gather(송신버퍼의 시작 주소, 송신버퍼의 원소 갯수, 송신 버퍼 원소의 데이터 타입, 
+    //            수신 버퍼의 주소, 수신할 원소의 갯수, 수신 버퍼 원소의 데이터 타입, 수신(루트)프로세스의 랭크, 커뮤니케이터)
+    MPI_Allgather(&send_msg, 1, MPI_INT, buff, 1, MPI_INT, MPI_COMM_WORLD);
+
+    // 프로세스 j의 데이터 -> 모든 수신 버퍼 j 번째 블록에 저장
+    cout << "[AFTER] RANK: " << rank << " AND I RECV (ALLGATHER): ";
+    for(int i = 0; i < size; i++) cout << buff[i] << " ";
+    cout << endl;
+    
+    // MPI 종료
+    MPI_Finalize();
+
+    return 0;
+}
