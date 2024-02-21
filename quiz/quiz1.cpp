d6500dff9d0c6cab5ecd93b75129c2154746fa88
alstjs37 <minsun987654321@gmail.com>
2024-02-21 13:33:14 +0900

    update function explain

--- quiz/quiz1.cpp
+++ quiz/quiz1.cpp
@@ -11,8 +11,9 @@ int ROOT = 0;
     
     sender process (rank = 1)이 ROOT process (rank = 0)에게 data1을 전달합니다.
     ROOT process (rank = 0)은 sender process (rank = 1)으로 부터 data1을 전달받아 그 값을 buff1[1]에 작성합니다.
-*/
-/*  [ HINT ]
+    즉, ROOT process (rank = 0)의 buff[3] = {68, 37, 69}를 buff[3] = {68, data1, 69} 으로 변경합니다.
+    
+    [ HINT ]
     배열의 이름은 배열의 시작주소를 담고 있습니다.
     배열의 n번째 인덱스는 (배열주소 + n) 형식으로 접근 가능합니다.
 */
@@ -22,33 +23,59 @@ void func1(int rank, int size) {
     int data1 = 75;
 
     if (rank == ROOT) {
-        // MPI_Recv(수신할 데이터를 저장할 버퍼의 시작 주소, 수신할 데이터의 개수, 수신할 데이터의 타입, 
-        //          송신자 프로세스의 랭크, 송신자가 메시지에 부여한 태그, 커뮤니케이터,
-        //          메세지의 상태를 나타내는 MPI_Status 구조체에 대한 포인터, 필요하지 않을 시 MPI_STATUS_IGNORE를 사용할 수 있음)
-        MPI_Recv(buff1+1, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        /* 코드를 작성해 주세요 (1줄) */
+
     } else if(rank == sender) {
-        // MPI_Send(전송할 데이터의 시작 주소, 전송할 데이터의 갯수, 전송할 데이터 타입, 목적지 프로세서 랭크, 메세지를 식별하는 태그, 커뮤니케이터)
-        MPI_Send(&data1, 1, MPI_INT, ROOT, 0, MPI_COMM_WORLD);
+        /* 코드를 작성해 주세요 (1줄) */
+
     }
 }
 
+/*  [ func2 설명 ]
+    MPI 함수 중 Collective Communication 함수를 사용합니다.
+
+    함수를 사용하여,
+    process rank: 0은 data2[0]을 ROOT process (rank = 0)의 buff[0]에,
+    process rank: 1은 data2[1]을 ROOT process (rank = 0)의 buff[1]에,
+    process rank: 2은 data2[2]을 ROOT process (rank = 0)의 buff[2]에 보냅니다.
+
+    즉, 각 프로세스가 가지고 있는 값을 ROOT 프로세스에 하나의 값 (buff)에 모으는 함수를 사용합니다.
+
+    [ HINT ]
+    배열의 이름은 배열의 시작주소를 담고 있습니다.
+    배열의 n번째 인덱스는 (배열주소 + n) 형식으로 접근 가능합니다.
+*/
 int buff2[3];
 void func2(int rank, int size) {
     int data2[3] = {70, 73, 71};
 
-    // MPI_Gather(송신 버퍼의 시작 주소, 송신 버퍼의 원소 갯수, 송신 버퍼 원소의 데이터 타입, 
-    //            수신 버퍼의 주소, 수신할 원소의 갯수, 수신 버퍼 원소의 데이터 타입, 수신(루트)프로세스의 랭크, 커뮤니케이터)
-    MPI_Gather(data2+rank, 1, MPI_INT, buff2, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+    /* 코드를 작성해 주세요 (1줄) */
+
 }
 
+/*  [ func3 설명 ]
+    MPI 함수 중 Collective Communication 함수를 사용합니다.
+
+    함수를 사용하여,
+    process rank: 0은 ( data2[0], data2[1] )을 모든 process (rank = 0, 1, 2)의 buff[0], buff[1]에,
+    process rank: 1은 ( data2[2], data2[3] )을 모든 process (rank = 0, 1, 2)의 buff[2], buff[3]에,
+    process rank: 2은 ( data2[4], data2[5] )을 모든 process (rank = 0, 1, 2)의 buff[4], buff[5]에 보냅니다.
+
+    즉, 각 프로세스가 가지고 있는 값을 모든 프로세스에 하나의 값 (buff)에 모으는 함수를 사용합니다.
+
+    [ HINT ]
+    배열의 이름은 배열의 시작주소를 담고 있습니다.
+    배열의 n번째 인덱스는 (배열주소 + n) 형식으로 접근 가능합니다.
+*/
 int buff3[6];
 void func3(int rank, int size) {
     int data3[6] = {72, 84, 73, 78, 71, 33};
-    // MPI_Allgather(송신 버퍼의 시작 주소, 송신 버퍼의 원소 개수, 송신 버퍼의 데이터 타입, 
-    //               수신 버퍼의 주소, 각 프로세스로부터 수신된 데이터 갯수, 수신버퍼 데이터 타입, 커뮤니케이터)
-    MPI_Allgather(data3+rank*2, 2, MPI_INT, buff3, 2, MPI_INT, MPI_COMM_WORLD);
+    
+    /* 코드를 작성해 주세요 (1줄) */
+
 }
 
+/* 결과를 출력해주는 함수 입니다. */
 void printResult(int rank, int size) {
     int result[12] = {0,};
 
@@ -77,13 +104,15 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // 프로세스는 3개를 사용해주세요
+    // 프로세스는 3개를 사용해 주세요
     if (size != 3) {cout << "[ERROR] INCORRECT NUMBER OF PROCESS " << endl; return 0;}
 
+    // func1, func2, func3을 작성해 주세요.
     func1(rank, size);
     func2(rank, size);
     func3(rank, size);
 
+    // 결과를 출력해주는 함수 입니다.
     printResult(rank, size);
 
     // MPI 종료
